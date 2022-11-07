c9a85e08d905dd3e300cec6a9a68521c85f3bed9
Eyakhaled <73254475+eyakhaled@users.noreply.github.com>
2022-11-07 13:15:59 +0100

    up

--- src/test/java/tn/esprit/rh/achat/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-package tn.esprit.rh.achat.services;
+/*package tn.esprit.rh.achat.services;
 
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -60,7 +60,7 @@ class StockServiceImplTest {
     }
 
 
-}
+}*/
 
 
 
