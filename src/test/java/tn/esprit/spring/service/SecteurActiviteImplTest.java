a455255b3f493cdce7a284d4cfac8d13d6076eda
Tabarki youssef <73352847+youssef2404@users.noreply.github.com>
2022-11-13 08:32:48 +0000

    Add files via upload

--- src/test/java/tn/esprit/spring/service/SecteurActiviteImplTest.java
+++ src/test/java/tn/esprit/spring/service/SecteurActiviteImplTest.java
@@ -1,21 +1,19 @@
-package tn.esprit.spring.service;
-
-import org.apache.logging.log4j.LogManager;
+package com.esprit.examen.services;
+import org.apache.logging.log4j.LogManager; 
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
 import com.esprit.examen.services.ISecteurActiviteService;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -48,23 +46,25 @@ public class SecteurActiviteImplTest {
 		
 		
 		
-		//Count missions a make sure the return is  not null
+		/*  //Count missions a make sure the return is  not null
 		@Test
 		public void testcountSecteurActivite() {
 		long nbrms = secteurActiviteRepository.count();
 		assertNotNull(nbrms);
 		l.info("Le Nombre des secteurActivite est :");
 		l.info(nbrms);
-		}
+		}*/
+		
+		 
 		
 		
 		
 		// Make sure the Database is not Nulls
 		@Test
-		public void testListMission() {
-		List<SecteurActivite> e = (List<SecteurActivite>) secteurActiviteRepository.findAll();
-		assertThat(e).size().isPositive();
-		l.info( "> 0");
+
+		public void testRetrieveAllSecteurActivite() {
+			List<SecteurActivite> listSecteurActivite = iSecteurActiviteService.retrieveAllSecteurActivite();
+			Assertions.assertEquals(0, listSecteurActivite.size());
 		}
 		
 		
@@ -72,11 +72,13 @@ public class SecteurActiviteImplTest {
 		
 		//delete a mission (id=2).
 		
-				@Test
-				public void deleteMission() {
-					long id = 2;
-					iSecteurActiviteService.deleteSecteurActivite(id);
-				}
+		@Test
+		public void testDeleteSecteurActivite() {
+			SecteurActivite o = new SecteurActivite("aaaaa", "bbbbb");
+			SecteurActivite savedSecteurActivite= iSecteurActiviteService.addSecteurActivite(o);
+			iSecteurActiviteService.deleteSecteurActivite(savedSecteurActivite.getIdSecteurActivite());
+			assertNull(iSecteurActiviteService.retrieveSecteurActivite(savedSecteurActivite.getIdSecteurActivite()));
+		}
 				
 
 	
